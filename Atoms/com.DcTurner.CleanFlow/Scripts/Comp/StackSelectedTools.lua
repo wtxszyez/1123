d41e8355dd18864eef9444c9b405bab5e7837d29
Alex Bogomolov <mail@abogomolov.com>
2019-10-18 09:08:46 +0300

    add Fusion 16 compatibility to CleanFlow

--- Atoms/com.DcTurner.CleanFlow/Scripts/Comp/StackSelectedTools.lua
+++ Atoms/com.DcTurner.CleanFlow/Scripts/Comp/StackSelectedTools.lua
@@ -1,19 +1,43 @@
-fl = comp.CurrentFrame.FlowView
+-- The script to align nodes vertically
+-- Based on original script by DC Turner LineSelectedTools.lua
+-- Change list by Alex Bogomolov (mail@abogomolov.com):
+--   code refactoring
+--   floor node positioning check
+--   add Fu16 compatibility 
+--   add configuration file to launch the script with ALT+V
+--
+-- Known issue: if the Node is set to Arrange to grid, and Y pos is less then 0, aligned position will shift to 1 on Y axis
 
-comp:StartUndo("Stack Tools")
-comp:Lock()
-_SEL = comp:GetToolList(true)
-_A = comp.ActiveTool
-if (_A == nil) then
-	_A = _SEL[1]
-end
-_AX,_AY = fl:GetPos(_A)
+flow = comp.CurrentFrame.FlowView
 
-for k,v in pairs(_SEL) do
-	if (v~= _A) then
-		_TEMP_X, _TEMP_Y = fl:GetPos(v)
-		fl:SetPos(v, _AX, _TEMP_Y)
-	end
+comp:StartUndo("Align Tools Vertically")
+selected_tools = comp:GetToolList(true)
+active_tool = comp.ActiveTool
+
+if not active_tool then
+    active_tool = selected_tools[1]
 end
-comp:Unlock()
+
+active_X, active_Y = flow:GetPos(active_tool)
+-- print(active_X.." - ".. active_Y)
+
+if fu.Version >= 16 then
+    active_Y = math.floor(active_Y)
+    flow:SetPos(active_tool, active_X, active_Y)
+end
+
+for num, tool in pairs(selected_tools) do
+    _TEMP_X, TEMP_Y = flow:GetPos(tool)
+    if tool ~= active_tool then
+        -- check if nodes have the same Y value
+        -- floor the values for switching between horiz and vertical allignment
+        if math.floor(TEMP_Y) == math.floor(active_Y) then 
+            newpos = TEMP_Y + (num - 1)
+        else
+            newpos = TEMP_Y
+        end
+        flow:SetPos(tool, active_X, newpos)
+    end
+end
 comp:EndUndo()
+
