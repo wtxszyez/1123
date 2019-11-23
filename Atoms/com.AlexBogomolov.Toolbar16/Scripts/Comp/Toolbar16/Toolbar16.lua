53e22f71e0666d1fd46744639db3b5486f64e023
Alex Bogomolov <mail@abogomolov.com>
2019-11-24 02:48:04 +0300

    add toggle TimeView, update description

--- Atoms/com.AlexBogomolov.Toolbar16/Scripts/Comp/Toolbar16/Toolbar16.lua
+++ Atoms/com.AlexBogomolov.Toolbar16/Scripts/Comp/Toolbar16/Toolbar16.lua
@@ -1,16 +1,23 @@
 --[[
+v 2.3
+-- add close preferences button
+-- add initial window offset based on left viewer window size
+-- add toggle TimeVew button
+-- add optional launch on mouse position
+    * if you need to launch the tool at custom position, first set prefs to launch at mouse pos, restart the script
+    * then open tool preferences and set Save Position
+-- add Fusion9 style View Bar with 5 simple layout presets
+-- click Add Toolbars! button to launch Customize Toolbars dialogue
 v 2.0 
     -- add preferences for save position and stay on top of all windows
     -- now properly working with 3D viewers
 v 1.3 add some working buttons 2019-05-21
--- partial implementation for Fusion 16 by Alex Bogomolov 
+-- partial implementation for Fusion 16 by Alex Bogomolov mail@abogomolov.com
+-- general discussion and screenshots: https://www.steakunderwater.com/wesuckless/viewtopic.php?p=23876#p23876
+   web: https://abogomolov.com
+   donate: https://paypal.me/aabogomolov
 v 1.0 Initial release 2019-01-21
--- original sample script and icons by Andrew Hazelden 
-Email:  andrew@andrewhazelden.com
-        mail@abogomolov.com
-Web: www.andrewhazelden.com
-     https://abogomolov.com
-     https://paypal.me/aabogomolov
+-- original sample script and icons by Andrew Hazelden. Thanks a lot for your invaluable help!
 ]]
 
 ui = fu.UIManager
@@ -106,7 +113,7 @@ function get_window_xy()
         print('launching at mouse position')
         return fu.MouseX, fu.MouseY
     else
-        local leftOffset = main_window_dimensions.Width*(1-.90)
+        local leftOffset = main_window_dimensions.Width*.12
         print(leftOffset)
         posX = main_window_dimensions.Width / 2 - leftOffset
         TBoffset = 35 -- fusion 9 offset
@@ -126,7 +133,7 @@ function show_ui()
     show_on_top = get_state('Toolbar16.OnTop')
     _init('left')
     -- check if window exists
-    width, height = 750,26
+    width, height = 770,26
     iconsMedium = {16,26}
     iconsMediumLong = {34,26}
     x, y = get_window_xy()
@@ -402,6 +409,14 @@ function show_ui()
                         Icon = ui:Icon{File = 'Scripts:/Comp/Toolbar16/Icons/PT_Layout05.png'},
                     },                             
                     ui:Button{
+                        ID = 'TimeViewButton',
+                        Flat = true,
+                        IconSize = {16,16},
+                        MinimumSize = iconsMedium,
+                        Checkable = false,
+                        Icon = ui:Icon{File = 'Scripts:/Comp/Toolbar16/Icons/PT_TimeView.png'},
+                    },                             
+                    ui:Button{
                         ID = 'CloseButton',
                         Text = 'Exit',
                         Flat = false,
@@ -473,7 +488,7 @@ function show_prefs_window(pos)
         ID = 'TBPrefs',
         TargetID = 'TBPrefs',
         WindowFlags = {SplashScreen = true, NoDropShadowWindowHint = true, WindowStaysOnTopHint = false},
-        Geometry = {prefx +125, prefy+offsetY, 250, 100},
+        Geometry = {prefx +137, prefy+offsetY, 250, 100},
             ui:VGroup{
             ID = 'prefs',
                 ui:HGroup{
@@ -801,7 +816,9 @@ function win.On.Layout05.Clicked(ev)
     comp:DoAction("Fusion_View_Show", {view = "Viewer2", show = true})
     comp:DoAction("Fusion_View_Show", {view = "Inspector", show = false})
 end
-
+function win.On.TimeViewButton.Clicked(ev)
+    comp:DoAction("Fusion_View_Show", {view = "Time"})
+end
 
 
 
